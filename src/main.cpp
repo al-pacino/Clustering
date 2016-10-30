b84b43d659438bb49e11160cd887b9b65b1ada3f
Anton Todua <toduaanton@yandex.ru>
2016-10-30 20:46:01 +0300

    implement parallel pam

--- src/main.cpp
+++ src/main.cpp
@@ -15,87 +15,176 @@ using namespace std;
 #include <DissimilarityMatrix.h>
 #include <PartitioningAroundMedoids.h>
 
-typedef CVector2d<float> CVector;
-typedef CVector::DistanceType DistanceType;
+typedef float DistanceType;
+typedef CVector2d<DistanceType> CVector;
 typedef CDissimilarityMatrix<DistanceType> DissimilarityMatrixType;
+
+///////////////////////////////////////////////////////////////////////////////
+
+static_assert( sizeof( size_t ) <= sizeof( uint32_t ),
+	"invalid: sizeof( size_t ) <= sizeof( uint32_t )" );
+
+struct CObjectMedoidDistance {
+	uint32_t Object;
+	uint32_t Medoid;
+	DistanceType Distance;
+
+	CObjectMedoidDistance() :
+		Object( 0 ),
+		Medoid( 0 ),
+		Distance( 0 )
+	{
+	}
+
+	void AllReduce();
+
+private:
+	static MPI_Datatype datatype();
+	static MPI_Op op();
+	static void MPIAPI objectMedoidDistanceMin(
+		CObjectMedoidDistance* in, CObjectMedoidDistance* inout,
+		int* length, MPI_Datatype* /*type*/ );
+};
+
+void CObjectMedoidDistance::AllReduce()
+{
+	MpiCheck( MPI_Allreduce( MPI_IN_PLACE, this, 1 /* count */, datatype(), op(), MPI_COMM_WORLD ),
+		"MPI_Allreduce for CObjectMedoidDistance" );
+}
+
+MPI_Datatype CObjectMedoidDistance::datatype()
+{
+	static MPI_Datatype type = MPI_DATATYPE_NULL;
+	if( type == MPI_DATATYPE_NULL ) {
+		const int count = 3;
+		int blocklengths[count] = { 1, 1, 1 };
+		MPI_Datatype types[count] = { MPI_UINT32_T, MPI_UINT32_T, MPI_FLOAT };
+		MPI_Aint offsets[count] = {
+			offsetof( CObjectMedoidDistance, Object ),
+			offsetof( CObjectMedoidDistance, Medoid ),
+			offsetof( CObjectMedoidDistance, Distance ) };
+		MpiCheck( MPI_Type_create_struct( count, blocklengths, offsets, types, &type ),
+			"MPI_Type_create_struct for CObjectMedoidDistance" );
+		MpiCheck( MPI_Type_commit( &type ), "MPI_Type_commit for CObjectMedoidDistance" );
+	}
+	return type;
+}
+
+MPI_Op CObjectMedoidDistance::op()
+{
+	static MPI_Op op = MPI_OP_NULL;
+	if( op == MPI_OP_NULL ) {
+		MpiCheck( MPI_Op_create( (MPI_User_function*)objectMedoidDistanceMin,
+			false /* commute */, &op ), "MPI_Op_create for CObjectMedoidDistance" );
+	}
+	return op;
+}
+
+void MPIAPI CObjectMedoidDistance::objectMedoidDistanceMin(
+	CObjectMedoidDistance* in, CObjectMedoidDistance* inout,
+	int* length, MPI_Datatype* /*type*/ )
+{
+	for( int i = 0; i < *length; i++ ) {
+		if( in[i].Distance < inout[i].Distance ) {
+			inout[i] = in[i];
+		}
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void CalcBeginEndObjects(
+	const size_t numberOfObjects, const size_t numberOfProcess, const size_t rank,
+	size_t& beginObject, size_t& endObject )
+{
+	const size_t objectsPerProcess = numberOfObjects / numberOfProcess;
+	const size_t additionalObjects = numberOfObjects % numberOfProcess;
+	beginObject = objectsPerProcess * rank + min( rank, additionalObjects );
+	endObject = beginObject + objectsPerProcess;
+	if( rank < additionalObjects ) {
+		endObject++;
+	}
+}
 
 void DoPam( const size_t numberOfClusters, const DissimilarityMatrixType& matrix )
 {
-	CPartitioningAroundMedois<DissimilarityMatrixType> pam( matrix, numberOfClusters );
+	typedef CPartitioningAroundMedois<DissimilarityMatrixType> PamType;
+	PamType pam( matrix, numberOfClusters );
 
-	// Initializing
-	cout << "Building...0" << endl;
-	size_t centralObject = 0;
-	CVector::DistanceType minDistance = pam.FindObjectDistanceToAll( 0 );
-	for( size_t object = 1; object < pam.NumberOfObjects(); object++ ) {
-		const CVector::DistanceType distance = pam.FindObjectDistanceToAll( object );
-		if( distance < minDistance ) {
-			minDistance = distance;
-			centralObject = object;
-		}
-	}
-	pam.AddMedoid( centralObject );
+	size_t objectBegin = 0;
+	size_t objectEnd = 0;
+	CalcBeginEndObjects( pam.NumberOfObjects(),
+		CMpiInitializer::NumberOfProccess(), CMpiInitializer::Rank(),
+		objectBegin, objectEnd );
 
-	// Building
-	for( size_t i = 1; i < pam.NumberOfClusters(); i++ ) {
+	CObjectMedoidDistance best;
+
+	// Building and Initializing
+	for( size_t i = 0; i < pam.NumberOfClusters(); i++ ) {
+#ifdef _DEBUG
 		cout << "Building..." << i << endl;
-
-		size_t medoid = pam.NumberOfObjects();
-		CVector::DistanceType maxDistance = 0;
-		for( size_t object = 0; object < pam.NumberOfObjects(); object++ ) {
+#endif
+		best.Distance = numeric_limits<DistanceType>::max();
+		best.Object = objectBegin;
+		for( size_t object = objectBegin; object < objectEnd; object++ ) {
 			if( pam.IsMedoid( object ) ) {
 				continue; // if object is medoid
 			}
 
-			const CVector::DistanceType distance = pam.AddMedoidProfit( object );
-			if( distance > maxDistance ) {
-				maxDistance = distance;
-				medoid = object;
+			const DistanceType distance = ( pam.State() == PamType::Initializing ) ?
+				pam.FindObjectDistanceToAll( object ) : -pam.AddMedoidProfit( object );
+
+			if( distance < best.Distance ) {
+				best.Distance = distance;
+				best.Object = object;
 			}
 		}
-		pam.AddMedoid( medoid );
+		best.AllReduce();
+		pam.AddMedoid( best.Object );
 	}
-
-	cout << endl;
 
 	// Swapping
 	for( size_t iteration = 0; iteration < 1000; iteration++ ) {
+#ifdef _DEBUG
 		cout << "Swapping..." << iteration << endl;
+#endif
+		best.Distance = 0;
+		best.Medoid = pam.Medoids().front();
+		best.Object = objectBegin;
 
-		CVector::DistanceType bestResult = 0;
-		size_t bestMedoid = pam.NumberOfObjects();
-		size_t bestObject = pam.NumberOfObjects();
-
-		for( const size_t medoid : pam.Medoids() ) {
-			for( size_t object = 0; object < pam.NumberOfObjects(); object++ ) {
-				if( pam.IsMedoid( object ) ) {
-					continue; // if object is medoid
-				}
-
-				CVector::DistanceType result = pam.SwapResult( medoid, object );
-				if( result < bestResult ) {
-					bestResult = result;
-					bestMedoid = medoid;
-					bestObject = object;
+		for( size_t object = objectBegin; object < objectEnd; object++ ) {
+			if( pam.IsMedoid( object ) ) {
+				continue; // if object is medoid
+			}
+			for( const size_t medoid : pam.Medoids() ) {
+				DistanceType distance = pam.SwapResult( medoid, object );
+				if( distance < best.Distance ) {
+					best.Distance = distance;
+					best.Medoid = medoid;
+					best.Object = object;
 				}
 			}
 		}
 
-		if( bestResult < 0 ) {
-			pam.Swap( bestMedoid, bestObject );
+		best.AllReduce();
+
+		if( best.Distance < 0 ) {
+			pam.Swap( best.Medoid, best.Object );
 		} else {
 			break;
 		}
 	}
 
-	cout << endl;
-
-	unordered_map<size_t, size_t> medoidToClusterId;
-	for( size_t object = 0; object < pam.NumberOfObjects(); object++ ) {
-		auto pair = medoidToClusterId.insert(
-			make_pair( pam.ObjectMedoids()[object], medoidToClusterId.size() ) );
-		cout << object << "\t" << pair.first->second << endl;
+#ifdef _DEBUG
+	if( CMpiInitializer::Rank() == 0 ) {
+		unordered_map<size_t, size_t> medoidToClusterId;
+		for( size_t object = 0; object < pam.NumberOfObjects(); object++ ) {
+			auto pair = medoidToClusterId.insert(
+				make_pair( pam.ObjectMedoids()[object], medoidToClusterId.size() ) );
+			cout << object << "\t" << pair.first->second << endl;
+		}
 	}
+#endif
 }
 
 void DoMain( const int argc, const char* const argv[] )
